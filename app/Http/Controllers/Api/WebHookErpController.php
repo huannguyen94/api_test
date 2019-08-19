d152c542dffa362cc88c700236067bf74118c743
DiepBap <thanhdiep9x2@gmail.com>
2019-08-19 10:20:18 +0700

    sua api

--- app/Http/Controllers/Api/WebHookErpController.php
+++ app/Http/Controllers/Api/WebHookErpController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Repositories\Seats\SeatRepository;
+use App\Repositories\Trips\GetTripInfoRepository;
 
 
 class WebHookErpController extends Controller
 {
-	public function __construct(SeatRepository $SeatRepository ){
+	public function __construct(SeatRepository $SeatRepository,GetTripInfoRepository $getTripInfoRepository ){
 		
-		$this->SeatRepository = $SeatRepository;
+        $this->SeatRepository        = $SeatRepository;
+        $this->getTripInfoRepository = $getTripInfoRepository;
 	}
     public function webHookErp( Request $request,$id){
 
