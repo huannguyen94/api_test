8f3612cf263d78a74b7cb219f062e727c8a1d9a3
HuanNguyen <huannguyen0511@gmail.com>
2020-02-21 15:18:04 +0700

    huan

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -11,17 +11,33 @@ class UserController extends Controller
 {
     public function getListingUser(Request $request)
     {
-    	$data_pb = DB::table('admin_lv2_user')->get();
-    	foreach ($data_pb as $key => $value) {
+    	$limit = isset($request->limit) ? intval($request->limit) : 50;
+        if($limit > 100) {
+            $limit = 100;
+        }
+
+        $page = isset($request->page) ? intval($request->page) : '';
+
+		$data_user      = DB::table('admin_lv2_user')->where('adm_group_id',2)->paginate($limit);
+		$data_chi_nhanh = DB::table('chi_nhanh')->get();
+
+		$chi_nhanh = [];
+		foreach ($data_chi_nhanh as $key => $value1) {
+			$chi_nhanh[$value1->cn_id] = $value1->cn_name;
+		}
+
+    	foreach ($data_user as $key => $value) {
 	    	$response[] = [	
 	    		"user"=>[
-					"id"   =>$value->adm_id,
-					"name" =>$value->adm_name,
-					"ma"   =>$value->adm_ma,
+					"id"           => $value->adm_id,
+					"name"         => $value->adm_name,
+					"phone"        => $value->adm_phone,
+					"noi_lam_viec" => isset($chi_nhanh[$value->adm_noi_lam_viec]) ? $chi_nhanh[$value->adm_noi_lam_viec] : "",
+					"vp_kt"        => ''
 	    		]
 	    	];
     	}
-    	return response()->json($response);
+        return response()->json($response);
     }
 
 }
